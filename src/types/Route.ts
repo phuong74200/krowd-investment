c162d2fa1e5fae1dafc0320d78d399072bc7ad2d
phuongmt@nexondv.com <phuongmt@nexondv.com>
2023-05-26 15:26:00 +0700

    change name case

--- src/types/Route.ts
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from 'react';
-import { NonIndexRouteObject } from 'react-router-dom';
-
-export interface AuthRouteObject extends NonIndexRouteObject {
-  layout?: (props: PropsWithChildren) => JSX.Element;
-  children?: AuthRouteObject[];
-  child?: AuthRouteObject;
-  title?: string;
-}

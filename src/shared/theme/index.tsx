6e5872042dc5b086480c76dbe7f3ac6f43e1010d
phuong74200 <phuong74200@gmail.com>
2023-06-10 13:06:04 +0700

    add override components

---
+++ src/shared/theme/index.tsx
@@ -0,0 +1,55 @@
+import { ReactNode, useMemo } from 'react';
+import {
+  createTheme,
+  CssBaseline,
+  StyledEngineProvider,
+  ThemeOptions,
+  ThemeProvider,
+} from '@mui/material';
+
+import useSettings from '@/shared/hooks/use-settings';
+
+import breakpoints from './breakpoints';
+import GlobalStyles from './global-styles';
+// import componentsOverride from './overrides';
+import palette from './palette';
+import shadows, { customShadows } from './shadows';
+import shape from './shape';
+import typography from './typography';
+
+type ThemeConfigProps = {
+  children: ReactNode;
+};
+
+export default function ThemeConfig({ children }: ThemeConfigProps) {
+  const { themeMode, themeDirection } = useSettings();
+  const isLight = themeMode === 'light';
+
+  const themeOptions: ThemeOptions = useMemo(
+    () => ({
+      palette: isLight
+        ? { ...palette.light, mode: 'light' }
+        : { ...palette.dark, mode: 'dark' },
+      shape,
+      typography,
+      breakpoints,
+      direction: themeDirection,
+      shadows: isLight ? shadows.light : shadows.dark,
+      customShadows: isLight ? customShadows.light : customShadows.dark,
+    }),
+    [isLight, themeDirection]
+  );
+
+  const theme = createTheme(themeOptions);
+  // theme.components = componentsOverride(theme);
+
+  return (
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <GlobalStyles />
+        {children}
+      </ThemeProvider>
+    </StyledEngineProvider>
+  );
+}
